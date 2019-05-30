1e08593761deaac77af1197e0bff43d79b7b4eab
Fernando Lopes <fernando.lopes@mirumagency.com>
2019-05-30 12:10:53 -0300

    footer

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,12 +2,30 @@ import React from 'react'
 import { UserProvider } from '../../contexts/user'
 import { useTheme } from '../../contexts/theme'
 
-import { Grid, Paper, Switch, Typography, Grow } from '@material-ui/core'
+import {
+  Grid,
+  Paper,
+  Switch,
+  Typography,
+  Grow,
+  makeStyles,
+} from '@material-ui/core'
 
 import Info from '../../templates/user/Info'
 import Skill from '../../templates/user/Skill'
 
+const useStyles = makeStyles({
+  footer: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+  },
+})
+
 function Home(props) {
+  const classes = useStyles()
+
   const { changePaletteType } = useTheme()
   return (
     <Grid container spacing={1}>
@@ -23,21 +41,22 @@ function Home(props) {
           </Grid>
         </Grow>
         <Grow in timeout={1500}>
-          <Grid item xs={12}>
-            <Paper style={{ padding: 20 }}>
-              <Typography variant="caption">Light theme</Typography>
-              <Switch
-                onChange={e => {
-                  changePaletteType(e.target.checked ? 'light' : 'dark')
-                }}
-              />
-            </Paper>
-          </Grid>
+          <Grid item xs={12} />
         </Grow>
       </UserProvider>
-      <Grid item xs={12}>
-        Footer
-      </Grid>
+      <footer className={classes.footer}>
+        <Paper style={{ padding: 20 }}>
+          <Typography variant="caption" display="block">
+            Footer
+          </Typography>
+          <Typography variant="caption">Light theme</Typography>
+          <Switch
+            onChange={e => {
+              changePaletteType(e.target.checked ? 'light' : 'dark')
+            }}
+          />
+        </Paper>
+      </footer>
     </Grid>
   )
 }
